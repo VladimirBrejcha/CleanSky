866684250ae8fa87e73ae81a26788d37b449ed40
Vladimir Korolev <python2python@icloud.com>
2019-05-07 20:46:43 +0300

    Initial Commit

---
+++ CleanSky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSky
+//
+//  Created by Vladimir Korolev on 07/05/2019.
+//  Copyright © 2019 Vladimir Brejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
