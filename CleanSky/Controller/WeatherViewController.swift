0ddb13b996d8ad2c748c1c29a7b928059798d6a6
Vladimir Korolev <python2python@icloud.com>
2019-05-08 15:16:18 +0300

    renamed icons and deleted not used ones added request for weather added json parsing added update ui methods

--- CleanSky/Controller/WeatherViewController.swift
+++ CleanSky/Controller/WeatherViewController.swift
@@ -10,38 +10,101 @@ import UIKit
 import Dropdowns
 import Alamofire
 import SwiftyJSON
-import CoreLocation
 
 class WeatherViewController: UIViewController {
     
     //Constants
-//    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-//    let APP_ID = "ac6a88be51624ad2b2799855bdf878d4"
-    let items = ["Moscow", "London", "New-York"]
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "ac6a88be51624ad2b2799855bdf878d4"
+    let items = ["Moscow", "London", "New York"]
+    let titleView: TitleView? = nil
     
     //instance variables
     var weatherDataModel = WeatherDataModel()
 
+    //Outlets
     @IBOutlet weak var currentWeatherImageView: UIImageView!
     @IBOutlet weak var currentWeatherLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    fileprivate func setupDropbox() {
         let titleView = TitleView(navigationController: navigationController!, title: "City", items: items)
-        Config.ArrowButton.Text.color = .black
         Config.ArrowButton.Text.selectedColor = .black
         Config.List.DefaultCell.Text.color = .black
         Config.List.DefaultCell.separatorColor = .gray
         Config.List.backgroundColor = .white
         titleView?.action = { [weak self] index in
-            print(self!)
-            print("select \(index)")
+            self?.changeCity(index)
         }
         
         navigationItem.titleView = titleView
     }
+    
+    //MARK: - Controller life cycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupDropbox()
+    }
+    
+    //MARK: - Networking
+    /***************************************************************/
+    func getWeatherData(url: String, parameters: [String : String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success")
+                
+                let weatherJSON: JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+                
+            } else {
+                print("Network error")
+                if let error = response.result.error {
+                    print("error - \(error)")
+                } else {
+                    print("some error")
+                }
+            }
+            
+        }
+    }
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    func updateWeatherData(json: JSON) {
+        print(json)
+        if let temperature = json["main"]["temp"].double {
+            let city = json["name"].stringValue
+            let condition = json["weather"][0]["id"].intValue
+            
+            weatherDataModel.temperature = Int(temperature - 273.15)
+            weatherDataModel.city = city
+            weatherDataModel.condition = condition
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: condition)
+            updateUIWithWeatherData()
+        } else {
+            print("Error loading data")
+        }
+        
+        
+    }
+    
+    func changeCity(_ newCityIndex: Int) {
+        let city = items[newCityIndex]
+        let locationProperties: [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url:WEATHER_URL, parameters: locationProperties)
+    }
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    func updateUIWithWeatherData() {
+        currentWeatherLabel.text = "\(weatherDataModel.temperature)"
+        currentWeatherImageView.image = UIImage(named: weatherDataModel.weatherIconName!)
+    }
 
 
 }
+
 
--- CleanSky/Model/WeatherDataModel.swift
+++ CleanSky/Model/WeatherDataModel.swift
@@ -23,40 +23,40 @@ class WeatherDataModel {
         switch (condition) {
             
         case 0...300 :
-            return "tstorm1"
+            return "storm"
             
         case 301...500 :
-            return "light_rain"
+            return "lightRain"
             
         case 501...600 :
-            return "shower3"
+            return "shower"
             
         case 601...700 :
-            return "snow4"
+            return "snow"
             
         case 701...771 :
             return "fog"
             
         case 772...799 :
-            return "tstorm3"
+            return "storm2"
             
         case 800 :
             return "sunny"
             
         case 801...804 :
-            return "cloudy2"
+            return "cloudy"
             
         case 900...903, 905...1000  :
-            return "tstorm3"
+            return "storm2"
             
         case 903 :
-            return "snow5"
+            return "snow2"
             
         case 904 :
             return "sunny"
             
         default :
-            return "dunno"
+            return "questionMark"
         }
         
     }
